bd9de4c2af2b952751f4927d535f26e3131336d3
sen <sen121108@icloud.com>
2022-11-21 11:29:16 +0900

    makeMigration

---
+++ app/Http/Controllers/InertiaTestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class InertiaTestController extends Controller
+{
+    public function index() {
+        return Inertia::render('Inertia/index');
+    }
+    public function show($id) {
+        return Inertia::render('Inertia/show',[
+            'id' => $id
+        ]);
+    }
+}
