c05c4e7c47e68039a37ca829788ef41062b4d7dd
sen <sen121108@icloud.com>
2022-11-22 19:43:45 +0900

    inertia-vue

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import GuestLayoutVue from '@/Layouts/GuestLayout.vue';
+import InputLabelVue from '@/Components/InputLabel.vue';
+
+</script>
+
+<template>
+<GuestLayoutVue>
+    <p>aaa</p>
+</GuestLayoutVue>
+<InputLabelVue>
+dd
+</InputLabelVue>
+
+</template>
